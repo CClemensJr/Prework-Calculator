37846e13d80de288b91df0b6daf693825f3e08aa
Charles Clemens <cclemensjr@gmail.com>
2019-01-07 12:30:18 -0800

    Added functions to handle user input

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,26 +6,28 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            UserInterface();
+
             // Ask for two numbers from the user
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Give me a couple numbers:");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Give me a couple numbers:");
 
             // Assign the numbers to variables
             // Double is used in case the user wants to add decimals
-            double firstNumber = Double.Parse(Console.ReadLine());
-            double secondNumber = Double.Parse(Console.ReadLine());
+            //double firstNumber = Double.Parse(Console.ReadLine());
+            //double secondNumber = Double.Parse(Console.ReadLine());
 
             // Perform the calculations and assign the results to a variable
-            double sum = Add(firstNumber, secondNumber);
-            double difference = Subtract(firstNumber, secondNumber);
-            double product = Multiply(firstNumber, secondNumber);
-            double quotient = Divide(firstNumber, secondNumber);
+            //double sum = Add(firstNumber, secondNumber);
+            //double difference = Subtract(firstNumber, secondNumber);
+            //double product = Multiply(firstNumber, secondNumber);
+            //double quotient = Divide(firstNumber, secondNumber);
 
             // Inform the user of the results
-            Console.WriteLine($"ADDITION: { firstNumber } + { secondNumber } = { sum }");
-            Console.WriteLine($"SUBTRACTION: { firstNumber } - { secondNumber } = { difference }");
-            Console.WriteLine($"MULTIPLICATION: { firstNumber } x { secondNumber } = { product }");
-            Console.WriteLine($"DIVISION: { firstNumber } / { secondNumber } = { quotient }");
+            //Console.WriteLine($"ADDITION: { firstNumber } + { secondNumber } = { sum }");
+            //Console.WriteLine($"SUBTRACTION: { firstNumber } - { secondNumber } = { difference }");
+            //Console.WriteLine($"MULTIPLICATION: { firstNumber } x { secondNumber } = { product }");
+            //Console.WriteLine($"DIVISION: { firstNumber } / { secondNumber } = { quotient }");
 
 
             // Keep the Console open until the user decides to close it
@@ -33,6 +35,90 @@ namespace Calculator
             Console.ReadLine();
         }
 
+
+        /**
+         *  INPUT HANDLERS
+         **/
+        static void UserInterface()
+        {
+            // Create a heading
+            int consoleWidth = Console.WindowWidth;
+            string heading = "Welcome to the Calculator. You got to keep it calculated!";
+
+            for (int j = 0; j < 5; j++)
+            {
+                if (j == 0 || j == 4)
+                {
+                    for (int i = 0; i < consoleWidth; i++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                else if (j == 2)
+                {
+                    Console.Write(String.Format("{0," + ((consoleWidth / 2) + (heading.Length / 2) + "}"), heading));
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            WhichFunction();
+        }
+
+
+        static void WhichFunction()
+        {
+            Console.WriteLine("Would you like to [A]dd, [S]ubtract, [M]ultiply, or [D]ivide?");
+            string function = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Now give me a couple numbers:");
+            // Assign the numbers to variables
+            // Double is used in case the user wants to add decimals
+            double firstNumber = Double.Parse(Console.ReadLine());
+            double secondNumber = Double.Parse(Console.ReadLine());
+
+            switch (function)
+            {
+                case "A":
+                    double sum = Add(firstNumber, secondNumber);
+
+                    Console.WriteLine($"ADDITION: { firstNumber } + { secondNumber } = { sum }");
+
+                    break;
+
+                case "S":
+                    double difference = Subtract(firstNumber, secondNumber);
+
+                    Console.WriteLine($"SUBTRACTION: { firstNumber } - { secondNumber } = { difference }");
+
+                    break;
+
+                case "M":
+                    double product = Multiply(firstNumber, secondNumber);
+
+                    Console.WriteLine($"MULTIPLICATION: { firstNumber } x { secondNumber } = { product }");
+
+                    break;
+
+                case "D":
+                    double quotient = Divide(firstNumber, secondNumber);
+
+                    Console.WriteLine($"DIVISION: { firstNumber } / { secondNumber } = { quotient }");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter A for Add, S for Subtract, M for Multiply, or D for Divide.");
+
+                    break;
+            }
+        }
+
+        /**
+         *  CALCULATIONS 
+         **/
         // Add two parameters together and return a solution
         static double Add(double number1, double number2)
         {
