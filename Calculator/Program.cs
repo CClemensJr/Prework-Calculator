afe7b9ecb345880bb648681ffd19faae19e7a98a
Charles Clemens <cclemensjr@gmail.com>
2019-01-07 10:53:30 -0800

    Created Add() functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,5 +8,14 @@ namespace Calculator
         {
             Console.WriteLine("Hello World!");
         }
+
+        // Add two parameters together and return a solution
+        // Double is used in case the user wants to add decimals
+        static double Add(double num1, double num2)
+        {
+            double product = num1 + num2;
+
+            return product;
+        }
     }
 }
