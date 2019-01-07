b1b1d34428aaf5738db9c755b772493ba7323371
Charles Clemens <cclemensjr@gmail.com>
2019-01-07 11:02:59 -0800

    Request user input in main

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,16 +6,28 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            // Ask for two numbers from the user
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me a couple numbers:");
+
+            // Assign the numbers to variables
+            double firstNumber = Double.Parse(Console.ReadLine());
+            double secondNumber = Double.Parse(Console.ReadLine());
+
+            // Perform the calculations and assign the results to a variable
+            double sum = Add(firstNumber, secondNumber);
+
+            // Inform the user of the results
+            Console.WriteLine($"Congratulations! { firstNumber } + { secondNumber } = { sum }");
         }
 
         // Add two parameters together and return a solution
         // Double is used in case the user wants to add decimals
-        static double Add(double num1, double num2)
+        static double Add(double number1, double number2)
         {
-            double product = num1 + num2;
+            double sum = number1 + number2;
 
-            return product;
+            return sum;
         }
     }
 }
